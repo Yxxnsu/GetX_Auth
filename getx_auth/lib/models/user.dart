db0e6cf52f962d8eac423ede6289feaeb2f6cc48
Yxxnsu <dustn0121@naver.com>
2021-08-09 23:14:28 +0900

    [ADD] user.dart

--- getx_auth/lib/models/user.dart
+++ getx_auth/lib/models/user.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class UserModel {
@@ -5,14 +7,20 @@ class UserModel {
   static const NAME = "name";
   static const EMAIL = "email";
 
-   late final String id;
-   late final String name;
-   late final String email;
+  String? id;
+  String? name;
+  String? email;
 
+  UserModel({
+    this.id,
+    this.name,
+    this.email,
+  });
 
-  UserModel.fromSnapshot(DocumentSnapshot snapshot) {
+  UserModel.fromSnapshot(DocumentSnapshot snapshot) {    
     name = snapshot.data()?[NAME];
     email = snapshot.data()?[EMAIL];
     id = snapshot.data()?[ID];
   }
 }
+
